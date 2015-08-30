b72f2a1a2f27f9a182674b77b8399737846e0c90
Naman Distro <ndistro@live.com>
2015-08-31 02:09:43 +0530

    Update shellshock_test.sh

--- shellshock_test.sh
+++ shellshock_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 EXITCODE=0
-
 # CVE-2014-6271
 CVE20146271=$(env 'x=() { :;}; echo vulnerable' 'BASH_FUNC_x()=() { :;}; echo vulnerable' bash -c "echo test" 2>&1 | grep 'vulnerable' | wc -l)
 
